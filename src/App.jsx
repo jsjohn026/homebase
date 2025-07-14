89fb37ba1c738584fb5dc6dec4fb88418aac8849
Jasmine John <jsjohn026@gmail.com>
2025-07-14 11:22:55 -0500

    build out work order submission form

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,46 @@ const Hello = (props) => {
   )
 }
 
+const WorkOrderForm = (props) => {
+  return (
+    <div>
+      <h3>Need a repair? Submit a work order here:</h3>
+      <form method="post" onSubmit={props.handleSubmit}>
+        <input type="text" placeholder='Request for repair' name="order" />
+        <input type="text" placeholder='additional notes' name="additionalNotes" />
+        <input type="date" name="dateAdded" />
+        <input type="text" placeholder='Name of person submitting request' name="submittedBy" />
+        <input type="email" placeholder='email' name="email" />
+        <input type="submit" name='submitBtn' value="submit" />
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ orderCount, setOrderCount ] = useState(0)
   const [ days, setDays ] = useState(5)
 
   const name = 'Jasmine'
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+    setOrderCount(orderCount + 1)
+  }
 
   return (
     <div>
       <h1>Welcome home!</h1>
       <Hello name={name} />
-      <p>You have {counter} work orders in progress.</p>
+      <p>You have {orderCount} work orders in progress.</p>
       <p>Rent is due in {days} days</p>
+      <WorkOrderForm handleSubmit={handleSubmit} />
+      <h3>Current Work Orders:</h3>
+      <ul>
+        <li>Sample order A</li>
+      </ul>
+
     </div>
   )
 }
