e457fbf6577d982c8fc5dfb7ddd67c9eb47d3e34
Jasmine John <jsjohn026@gmail.com>
2025-07-15 10:52:25 -0500

    style HomeBase app and work order request form

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 const Hello = (props) => {
   console.log(props)
   return (
-    <div>
-      <p>Hello {props.name}</p>
+    <div  className='header' >
+      <h3>Hello {props.name}, welcome home!</h3>
     </div>
   )
 }
 
 const WorkOrderForm = (props) => {
   return (
-    <div>
+    <div className='container' >
       <h3>Need a repair? Submit a work order here:</h3>
-      <form method="post" onSubmit={props.handleSubmit}>
-        <input type="text" placeholder='Request for repair' name="order" />
-        <input type="text" placeholder='additional notes' name="additionalNotes" />
-        <input type="date" name="dateAdded" />
-        <input type="text" placeholder='Name of person submitting request' name="submittedBy" />
-        <input type="email" placeholder='email' name="email" />
-        <input type="submit" name='submitBtn' value="submit" />
+      <form className='add-form' method="post" onSubmit={props.handleSubmit}>
+
+        <div className='form-control' >
+          <label>Repair</label>
+          <input type="text" placeholder='Request for repair' name="order" />
+          <input type="text" placeholder='Additional notes' name="additionalNotes" />          
+          <input type="text" placeholder='Name of person submitting request' name="submittedBy" />
+          <input type="email" placeholder='Email' name="email" />
+          <input type="date" name="dateAdded" />
+        </div>
+
+        <input type="submit" className='btn btn-block' name='submitBtn' value="Submit" />
       </form>
     </div>
   )
@@ -38,17 +44,19 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Welcome home!</h1>
+    <div className='container'>
+      <Header name={name} />
       <Hello name={name} />
+      
       <p>You have {orderCount} work orders in progress.</p>
       <p>Rent is due in {days} days</p>
+
       <WorkOrderForm handleSubmit={handleSubmit} />
+
       <h3>Current Work Orders:</h3>
       <ul>
         <li>Sample order A</li>
       </ul>
-
     </div>
   )
 }
