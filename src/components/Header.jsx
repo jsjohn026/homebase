cd2cd05a0dc74dbbf765a56491f658361a06565f
Jasmine John <jsjohn026@gmail.com>
2025-12-09 13:28:48 -0600

    Added username and logout handler to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
 import Button from './Button'
 import logo from '/images/white-high-res-logo-transparent.png'
 
-const Header = ({ onAdd, showAddForm }) => {
+const Header = ({ onAdd, showAddForm, username, onLogout }) => {
   return (
     <header className='header'>
       <img src={logo} className='logo' alt="Homebase logo" />
+      <h1>Work Orders</h1>
+      {username && <p>Welcome, {username}!</p>}
+
       <Button 
         color={showAddForm ? 'red' : 'green'} 
         text={showAddForm ? 'Close' : 'Add'} 
         onClick={onAdd} 
       />
+      {username && (
+        <Button 
+          color='gray'
+          text='Logout'
+          onClick={onLogout}
+        />
+      )}
     </header>
   )
 }
