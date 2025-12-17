93c2033ca625ce83f6320bbb28dcd5986d7ef6d2
Jasmine John <jsjohn026@gmail.com>
2025-12-17 16:45:50 -0600

    Adjusted styling to show logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,24 @@ import logo from '/images/white-high-res-logo-transparent.png'
 const Header = ({ onAdd, showAddForm, username, onLogout }) => {
   return (
     <header className='header'>
-      <img src={logo} className='logo' alt="Homebase logo" />
-      <h1>Work Orders</h1>
-      {username && <p>Welcome, {username}!</p>}
+      <div className="header-top">
+        <img src={logo} className='logo' alt="Homebase logo" />
+        {username && (
+          <Button 
+            color='gray'
+            text='Logout'
+            onClick={onLogout}
+          />
+        )}
+      </div>
+      
+      <h1>{username && <p>Welcome, {username}!</p>}</h1>
 
       <Button 
         color={showAddForm ? 'red' : 'green'} 
         text={showAddForm ? 'Close' : 'Add'} 
         onClick={onAdd} 
       />
-      {username && (
-        <Button 
-          color='gray'
-          text='Logout'
-          onClick={onLogout}
-        />
-      )}
     </header>
   )
 }
